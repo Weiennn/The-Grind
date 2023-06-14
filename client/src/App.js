70db47cfd05075bf2fb5d7196f0eaeb8e22db5a1
Originalidk <ryanpang321@gmail.com>
2023-06-14 16:22:14 +0800

    New issues with editting assignments

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ import Registration from "./pages/Registration";
 import Timer from "./pages/Timer"
 import Assignments from "./pages/Assignments"
 import NewAssignment from "./pages/NewAssignment"
-import UpdateAssignment from "./pages/UpdateAssignment"
 import { AuthContext } from "./helper/AuthContext";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -89,7 +88,6 @@ function App() {
             <Route path="/timer" element={<Timer />} />
             <Route path="/assignments" element={<Assignments />} />
             <Route path="/newAssignment" element={<NewAssignment />} />
-            <Route path="/updateAssignment/:assignmentId" element={<UpdateAssignment />} />
           </Routes>
         </Router>
       </AuthContext.Provider> 
