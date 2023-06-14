42b32ad921b6d77fd3ebac26ad4eec9023c521c3
weien <huangweien01@gmail.com>
2023-06-14 20:41:11 +0800

    Created Navbar component and moved the rest of the components there

--- client/src/App.js
+++ client/src/App.js
@@ -1,100 +1,27 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
-import Forum from "./pages/Forum";
-import NewPost from "./pages/NewPost";
-import Post from "./pages/Post";
-import Login from "./pages/Login";
-import Registration from "./pages/Registration";
-import Timer from "./pages/Timer"
-import Assignments from "./pages/Assignments"
-import NewAssignment from "./pages/NewAssignment"
-import UpdateAssignment from "./pages/UpdateAssignment"
-import { AuthContext } from "./helper/AuthContext";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Navbar from './pages/Navbar';
 
 function App() {
-  // State containing user login details
-  const [authState, setAuthState] = useState({
-    username: "",
-    id: 0,
-    status: false,
+
+  const defaultTheme = createTheme({
+    palette: {
+      mode: "light",
+      primary: {
+        main: "#38945e",
+      },
+      secondary: {
+        main: "#65ccb8",
+        contrastText: "rgba(0, 0, 0, 0.87)",
+      },
+    },
   });
-
-  // Runs when the page is being refreshed or loaded
-  useEffect(() => {
-    // Check if user has a valid token
-    axios
-      .get("http://localhost:3001/auth/auth", {
-        headers: {
-          accessToken: localStorage.getItem("accessToken"),
-        },
-      })
-      .then((response) => {
-      if (response.data.error) {
-        // Set login status to be false
-        setAuthState({...authState, status: false});
-      } else {
-        // Set login status to be true and set other user details
-        setAuthState({
-          username: response.data.username,
-          id: response.data.id,
-          status: true,
-        });
-      }
-    })
-  }, []);
-
-  // Function called when logging out
-  const logout = () => {
-    // Remove accessToken
-    localStorage.removeItem("accessToken");
-    // Set login status to be false as well as clear username and id
-    setAuthState({
-      username: "",
-      id: 0,
-      status: false,
-    });
-  };
 
   return (
     <div className="App">
-      <AuthContext.Provider value={{ authState, setAuthState }}>
-        <Router>
-          <Navbar />
-          <div className="navbar">
-            <Link to="newPost">Create new post</Link>
-            <Link to="/">Forum</Link>
-            <Link to="timer">Timer</Link>
-            {!authState.status ? (
-              <>
-                <Link to="login">Login</Link>
-                <Link to="registration">Registration</Link>
-              </>
-            ) : (
-              <>
-                <Link to="assignments">Assignments</Link>
-                <Link to="newAssignment">Create Assignment</Link>
-                <button onClick={logout}>Logout</button>
-              </>
-            )}
-
-            <h1>{authState.username}</h1>
-          </div>
-          <Routes>
-            <Route path="/" element={<Forum />} />
-            <Route path="/newPost" element={<NewPost />} />
-            <Route path="/post/:id" element={<Post />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/registration" element={<Registration />} />
-            <Route path="/timer" element={<Timer />} />
-            <Route path="/assignments" element={<Assignments />} />
-            <Route path="/newAssignment" element={<NewAssignment />} />
-            <Route path="/updateAssignment/:assignmentId" element={<UpdateAssignment />} />
-          </Routes>
-        </Router>
-      </AuthContext.Provider> 
+      <ThemeProvider theme={defaultTheme}>
+        <Navbar />
+      </ThemeProvider>
     </div>
   );
 }
